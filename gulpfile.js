a56335bf412ae8016d0982336c3acd321446cb5e
Мария <slinkywizard@mail.ru>
2021-02-04 20:04:12 +0300

    Сборка проекта, подготовка графики, верстка хедера

---
+++ gulpfile.js
@@ -0,0 +1,176 @@
+//"use strict";
+//const ghPages = require('gh-pages');
+//const path = require('path');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var plumber = require("gulp-plumber");
+var sourcemap = require("gulp-sourcemaps");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
+var csso = require("gulp-csso");
+var rename = require("gulp-rename");
+var webp = require("gulp-webp");
+var imagemin = require("gulp-imagemin");
+var svgstore = require("gulp-svgstore");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
+var del = require("del");
+var uglify = require("gulp-uglify");
+var pump = require("pump");
+
+gulp.task("css", function () {
+  return gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(csso())
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("copy_css", function () {
+  return gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer()
+    ]))
+    .pipe(gulp.dest("build/css"))
+    .pipe(server.stream());
+});
+
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({
+        optimizationLevel: 3
+      }),
+      imagemin.mozjpeg({
+        progressive: true
+      }),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({
+      quality: 90
+    }))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("sprite", function () {
+  return gulp.src(["source/img/icon-*.svg"])
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+      "source/fonts/**/*.{woff,woff2}",
+      "source/img/**",
+      "source/js/**",
+      "source/*.ico"
+    ], {
+      base: "source"
+    })
+    .pipe(gulp.dest("build"));
+});
+
+gulp.task("clean", function () {
+  return del("build");
+});
+
+/*
+gulp.task("js", function (done) {
+  pump([
+      gulp.src("source/js/*.js"),
+      gulp.dest("build/js")
+    ],
+    done
+  );
+});
+*/
+gulp.task("min-js", function (done) {
+  pump([
+      gulp.src("source/js/*.js"),
+      uglify(),
+      rename(function (path) {
+        path.basename += ".min";
+      }),
+      gulp.dest("build/js")
+    ],
+    done
+  );
+});
+
+gulp.task("server", function () {
+  server.init({
+    server: "build/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/sass/**/*.{sass,scss}", gulp.series("css"));
+  gulp.watch("source/*.html").on("change", server.reload);
+  gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
+  //gulp.watch("source/js/**/*.js", gulp.series("js"));
+  gulp.watch("source/js/**/*.js", gulp.series("min-js"));
+  gulp.watch("source/*.html", gulp.series("html", "refresh"));
+});
+
+gulp.task("refresh", function (done) {
+  server.reload();
+  done();
+});
+
+gulp.task("build", gulp.series("clean", "copy", "css", "html", "sprite", "copy_css", "min-js"));
+gulp.task("start", gulp.series("build", "server"));
+
+/*
+function deploy(cb) {
+  ghPages.publish(path.join(process.cwd(), './build'), cb);
+}
+exports.deploy = deploy;
+*/
+
+/*
+var imagemin = require("imagemin"); // The imagemin module.
+var webp = require("imagemin-webp"); // imagemin's WebP plugin.
+var outputFolder = "./img"; // Output folder
+var PNGImages = "./img/*.png"; // PNG images
+var JPEGImages = "./img/*.jpg"; // JPEG images
+
+imagemin([PNGImages], outputFolder, {
+  plugins: [webp({
+    lossless: true // Losslessly encode images
+  })]
+});
+
+imagemin([JPEGImages], outputFolder, {
+  plugins: [webp({
+    quality: 65 // Quality setting from 0 to 100
+  })]
+});
+*/
